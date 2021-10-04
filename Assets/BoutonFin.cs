1bf62cde84cc82312188574826ddbe142057a90e
Cryp3rone <Manach.Nicolas.mn@gmail.com>
2021-10-04 23:15:02 +0200

    Scene findejeu

---
+++ Assets/BoutonFin.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoutonFin : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
